ab15219c9cbefe8eedb8a1561643f7c376dadf17
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-04-15 17:11:31 +0200

    wrapper fix

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var dootify = require('dootify/process');
+var dootify = require('dootify');
+var objectAssign = require('object-assign');
+var loaderUtils = require('loader-utils');
+var DEFAULTS = dootify.DEFAULTS;
+var MINIFIER_DEFAULTS = dootify.MINIFIER_DEFAULTS;
 
 module.exports = function(source) {
   //TODO: cache id
   //var id = require.resolve(this.resource);
 
-  this.callback(null, dootify(source));
+  var opts = loaderUtils.parseQuery(this.query);
+
+  opts = objectAssign({}, opts, DEFAULTS);
+
+  if (opts.minifierOpts !== false) {
+    opts.minifierOpts = objectAssign({}, opts.minifierOpts, MINIFIER_DEFAULTS);
+  }
+
+  console.log(dootify.transform(source, opts));
+
+  return dootify.transform(source, opts);
 };
