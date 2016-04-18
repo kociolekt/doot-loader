9df584a23642b15f0eb4262010a1be9510f56fc7
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-04-18 17:27:44 +0200

    update doofity

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 var dootify = require('dootify');
-var objectAssign = require('object-assign');
-var loaderUtils = require('loader-utils');
-var DEFAULTS = dootify.DEFAULTS;
-var MINIFIER_DEFAULTS = dootify.MINIFIER_DEFAULTS;
 
 module.exports = function(source) {
-  //TODO: cache id
-  //var id = require.resolve(this.resource);
+  var id = require.resolve(this.resource);
 
-  var opts = loaderUtils.parseQuery(this.query);
-
-  opts = objectAssign({}, opts, DEFAULTS);
-
-  if (opts.minifierOpts !== false) {
-    opts.minifierOpts = objectAssign({}, opts.minifierOpts, MINIFIER_DEFAULTS);
-  }
-
-  console.log(dootify.transform(source, opts));
-
-  return dootify.transform(source, opts);
+  return dootify.process(dootify.compile(id, source));
 };
